daf0b9889daee00291cab469b7f8ff9953190c10
theoliverlear <sigwartholiver2@gmail.com>
2025-04-12 19:57:29 -0500

    Add Jupyter kernel setup for virtual environment in init_codespace.sh

--- Powerful-Data-Science/init_codespace.sh
+++ Powerful-Data-Science/init_codespace.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-set -e
+set -e  
 
 curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
 
-
 bash miniconda.sh -b -p "$HOME/miniconda"
-
 
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init bash
 
 conda create -y -n venv python=3.12
 
-
 conda activate venv
 
 pip install --upgrade pip
-pip install pandas numpy matplotlib scikit-learn tensorflow jupyter seaborn
+pip install pandas numpy matplotlib scikit-learn tensorflow jupyter seaborn ipykernel
+
+python -m ipykernel install --user --name=venv --display-name "Python (venv)"
